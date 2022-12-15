993a47977839bd5d26d6dd991c184195d83ebe4c
Estevao Pereira <estevaopp05@gmail.com>
2022-12-15 18:33:42 -0300

    first commit

---
+++ MarombaApi/Models/InfoPerson.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MarombaApi.Models
+{
+    public class InfoPerson
+    {
+        [Required]
+        private int UserId;
+        public decimal();
+    }
+}
